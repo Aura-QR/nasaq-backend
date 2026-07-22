97d67dabd6648d9800691dbb4a08b8b3992d03c4
Hamduy Salah <Hamdysalah@gmail.com>
2026-07-22 10:28:19 +0300

    fix: update CORS origin setting v2

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,11 @@ import { ResponseInterceptor } from './interceptors/response.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'true',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+app.enableCors({
+  origin: 'http://nasaq.185.170.196.120.sslip.io',
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+  credentials: true,
+});
 
   
   // Global validation pipe
